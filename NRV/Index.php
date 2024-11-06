1304fe5725b6c0ae401cd018c89a27fedad5c961
David <80073731+ByZorks@users.noreply.github.com>
2024-11-06 15:40:56 +0100

    changement noms

--- NRV/Index.php
+++ NRV/Index.php
@@ -3,13 +3,13 @@ declare(strict_types=1);
 
 require_once './vendor/autoload.php';
 
-use iutnc\deefy\repository\DeefyRepository;
-use iutnc\deefy\dispatch\Dispatcher;
+use \nrv\repository\NrvRepository;
+use nrv\dispatch\Dispatcher;
 
 session_start();
 
 try {
-    DeefyRepository::setConfig('config/db.config.ini');
+    NrvRepository::setConfig('config/db.config.ini');
 } catch (Exception $e) {
     print("Erreur de configuration : " . $e->getMessage());
 }
