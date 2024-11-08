5500730415dcb0a8d8307b000e8c10a1626070d4
David <80073731+ByZorks@users.noreply.github.com>
2024-11-08 17:28:32 +0100

    Tableau de bord.md + Script_BD.sql

---
+++ documents/Script_BD.sql
@@ -0,0 +1,305 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : jeu. 07 nov. 2024 à 10:32
+-- Version du serveur : 10.4.32-MariaDB
+-- Version de PHP : 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `nrv`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `artiste`
+--
+
+DROP TABLE IF EXISTS `artiste`;
+CREATE TABLE `artiste` (
+                           `id` int(200) NOT NULL AUTO_INCREMENT,
+                           `nom` varchar(50) NOT NULL,
+                           PRIMARY KEY(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `image`
+--
+
+DROP TABLE IF EXISTS `image`;
+CREATE TABLE `image` (
+                         `id` int(200) NOT NULL AUTO_INCREMENT,
+                         `url` varchar(50) NOT NULL,
+                         PRIMARY KEY(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `image2lieu`
+--
+
+DROP TABLE IF EXISTS `image2lieu`;
+CREATE TABLE `image2lieu` (
+                              `id_image` int(200) NOT NULL,
+                              `id_lieu` int(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `lieu`
+--
+
+DROP TABLE IF EXISTS `lieu`;
+CREATE TABLE `lieu` (
+                        `id` int(200) NOT NULL AUTO_INCREMENT,
+                        `nom` varchar(50) NOT NULL,
+                        `adresse` varchar(50) NOT NULL,
+                        `nbPlaceAssises` int(255) DEFAULT NULL,
+                        `nbPlacesDebout` int(255) DEFAULT NULL,
+                        PRIMARY KEY(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `soiree`
+--
+
+DROP TABLE IF EXISTS `soiree`;
+CREATE TABLE `soiree` (
+                          `id` int(200) NOT NULL AUTO_INCREMENT,
+                          `nom` varchar(50) NOT NULL,
+                          `thematique` varchar(50) DEFAULT NULL,
+                          `date` date DEFAULT NULL,
+                          `horaire_debut` time DEFAULT NULL,
+                          `horaire_fin` time DEFAULT NULL,
+                          `id_lieu` int(200) DEFAULT NULL,
+                          PRIMARY KEY(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `soiree2spectacle`
+--
+
+DROP TABLE IF EXISTS `soiree2spectacle`;
+CREATE TABLE `soiree2spectacle` (
+                                    `id_soiree` int(200) NOT NULL,
+                                    `id_spectacle` int(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `spectacle`
+--
+
+DROP TABLE IF EXISTS `spectacle`;
+CREATE TABLE `spectacle` (
+                             `id` int(200) NOT NULL AUTO_INCREMENT,
+                             `titre` varchar(50) NOT NULL,
+                             `description` varchar(200) DEFAULT NULL,
+                             `url` varchar(100) DEFAULT NULL,
+                             `horaire` time DEFAULT NULL,
+                             `duree` int(11) DEFAULT NULL,
+                             `style` varchar(50) DEFAULT NULL,
+                             PRIMARY KEY(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `spectacle2artiste`
+--
+
+DROP TABLE IF EXISTS `spectacle2artiste`;
+CREATE TABLE `spectacle2artiste` (
+                                     `id_spectacle` int(200) NOT NULL,
+                                     `id_artiste` int(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `spectacle2image`
+--
+
+DROP TABLE IF EXISTS `spectacle2image`;
+CREATE TABLE `spectacle2image` (
+                                   `id_spectacle` int(200) NOT NULL,
+                                   `id_image` int(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+                        `id` int(200) NOT NULL AUTO_INCREMENT,
+                        `email` varchar(50) NOT NULL,
+                        `hash` varchar(256) NOT NULL,
+                        `role` int(100) NOT NULL,
+                        PRIMARY KEY(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `image2lieu`
+--
+ALTER TABLE `image2lieu`
+    ADD PRIMARY KEY (`id_image`,`id_lieu`);
+
+ALTER TABLE `image2lieu`
+    ADD CONSTRAINT `image2lieu_ibfk_1` FOREIGN KEY (`id_image`) REFERENCES `image` (`id`),
+    ADD CONSTRAINT `image2lieu_ibfk_2` FOREIGN KEY (`id_lieu`) REFERENCES `lieu` (`id`);
+
+--
+-- Index pour la table `soiree2spectacle`
+--
+ALTER TABLE `soiree2spectacle`
+    ADD PRIMARY KEY (`id_soiree`,`id_spectacle`);
+
+ALTER TABLE `soiree2spectacle`
+    ADD CONSTRAINT `soiree2spectacle_ibfk_1` FOREIGN KEY (`id_soiree`) REFERENCES `soiree` (`id`),
+    ADD CONSTRAINT `soiree2spectacle_ibfk_2` FOREIGN KEY (`id_spectacle`) REFERENCES `spectacle` (`id`);
+
+--
+-- Index pour la table `spectacle2artiste`
+--
+ALTER TABLE `spectacle2artiste`
+    ADD PRIMARY KEY (`id_spectacle`,`id_artiste`);
+
+ALTER TABLE `spectacle2artiste`
+    ADD CONSTRAINT `spectacle2artiste_ibfk_1` FOREIGN KEY (`id_spectacle`) REFERENCES `spectacle` (`id`),
+    ADD CONSTRAINT `spectacle2artiste_ibfk_2` FOREIGN KEY (`id_artiste`) REFERENCES `artiste` (`id`);
+
+--
+-- Index pour la table `spectacle2image`
+--
+ALTER TABLE `spectacle2image`
+    ADD PRIMARY KEY (`id_spectacle`,`id_image`);
+
+ALTER TABLE `spectacle2image`
+    ADD CONSTRAINT `spectacle2image_ibfk_1` FOREIGN KEY (`id_spectacle`) REFERENCES `spectacle` (`id`),
+    ADD CONSTRAINT `spectacle2image_ibfk_2` FOREIGN KEY (`id_image`) REFERENCES `image` (`id`);
+
+INSERT INTO `soiree` (id, nom, thematique, date, horaire_debut, horaire_fin, id_lieu) VALUES
+    (1,'Best of rock','Rock','12-11-2024','19','23',1);
+INSERT INTO `soiree` (id, nom, thematique, date, horaire_debut, horaire_fin, id_lieu) VALUES
+    (2,'Best of country music','Country','13-11-2024','20','23',2);
+INSERT INTO `soiree` (id, nom, thematique, date, horaire_debut, horaire_fin, id_lieu) VALUES
+    (3,'Best of Reggae','Reggae','14-11-2024','18','22', 3);
+
+
+
+INSERT INTO spectacle (id, titre, description, url, horaire,duree, style) VALUES
+                                                                              (1, 'Jazz Band Live', 'A famous jazz band live in concert.', 'https://example.com/jazzlive', '20:00',2, 'Jazz'),
+                                                                              (2, 'Acoustic Jazz Trio', 'Smooth acoustic jazz trio.', 'https://example.com/acousticjazz', '21:00',3, 'Jazz'),
+                                                                              (3, 'Jazz Solo Piano', 'Relaxing solo piano jazz.', 'https://example.com/solopiano', '22:30',4, 'Jazz'),
+                                                                              (4, 'Rock Legends', 'Classic rock covers.', 'https://example.com/rocklegends', '20:30',1, 'Rock'),
+                                                                              (5, 'Indie Rock Band', 'Local indie rock band performance.', 'https://example.com/indierock', '22:00',2, 'Rock'),
+                                                                              (6, 'Blues Guitar', 'Blues guitar solo performance.', 'https://example.com/bluesguitar', '19:00',3, 'Blues'),
+                                                                              (7, 'Soulful Blues', 'Deep, soulful blues.', 'https://example.com/soulblues', '20:30',1, 'Blues'),
+                                                                              (8, 'Classical Quartet', 'String quartet performance.', 'https://example.com/classicalquartet', '19:45',2, 'Classical'),
+                                                                              (9, 'Electro DJ Set', 'Dance to electronic beats.', 'https://example.com/electro', '21:30',3, 'Electronic'),
+                                                                              (10, 'Techno Night', 'Non-stop techno music.', 'https://example.com/techno', '23:00',2, 'Electronic'),
+                                                                              (11, 'House Party', 'House music by top DJs.', 'https://example.com/house', '01:00',4, 'Electronic');
+
+INSERT INTO soiree2spectacle (id_soiree, id_spectacle) VALUES
+                                                           ('1','1'),
+                                                           ('2','2'),
+                                                           ('3','3');
+
+
+INSERT INTO artiste (id, nom) VALUES
+                                  (1, 'John Coltrane'), (2, 'Miles Davis'), (3, 'Bill Evans'),
+                                  (4, 'Jimmy Page'), (5, 'Robert Plant'),
+                                  (6, 'B.B. King'), (7, 'Muddy Waters'),
+                                  (8, 'Yo-Yo Ma'), (9, 'Daft Punk'),
+                                  (10, 'Carl Cox'), (11, 'David Guetta');
+
+
+
+INSERT INTO spectacle2artiste (id_spectacle, id_artiste) VALUES
+                                                             (1, 1), (1, 2),
+                                                             (2, 3), (3, 1),
+                                                             (4, 4), (5, 5),
+                                                             (6, 6), (7, 7),
+                                                             (8, 8),
+                                                             (9, 9), (10, 10), (11, 11);
+
+INSERT INTO lieu (id, nom, adresse, nbPlaceAssises, nbPlacesDebout) VALUES
+                                                                        (1, 'Le Jazz Club', '123 Jazz St', 100, 50),
+                                                                        (2, 'Rock Arena', '456 Rock Ave', 200, 120),
+                                                                        (3, 'Blues House', '789 Blues Rd', 80, 40),
+                                                                        (4, 'Grand Théâtre', '101 Classical Blvd', 200, 0),
+                                                                        (5, 'Electro Warehouse', '202 Techno Lane', 0, 10);
+
+
+INSERT INTO image (id, url) VALUES
+                                (1, 'https://example.com/images/jazz1.jpg'),
+                                (2, 'https://example.com/images/jazz2.jpg'),
+                                (3, 'https://example.com/images/jazz3.jpg'),
+                                (4, 'https://example.com/images/rock1.jpg'),
+                                (5, 'https://example.com/images/rock2.jpg'),
+                                (6, 'https://example.com/images/blues1.jpg'),
+                                (7, 'https://example.com/images/blues2.jpg'),
+                                (8, 'https://example.com/images/classical1.jpg'),
+                                (9, 'https://example.com/images/electro1.jpg'),
+                                (10, 'https://example.com/images/electro2.jpg'),
+                                (11, 'https://example.com/images/house1.jpg');
+
+
+
+INSERT INTO spectacle2image VALUES
+                                (1, 1), (2, 2), (3, 3),
+                                (4, 4), (5, 5),
+                                (6, 6), (7, 7),
+                                (8, 8),
+                                (9, 9), (10, 10), (11, 11);
+
+
+
+INSERT INTO image2lieu (id_image, id_lieu) VALUES
+                                               (1, 1), (2, 1), (3, 1),
+                                               (4, 2), (5, 2),
+                                               (6, 3), (7, 3),
+                                               (8, 4),
+                                               (9, 5), (10, 5), (11, 5);
+
+
+INSERT INTO user (id, email, hash, role) VALUES
+                                             (1, 'user1@example.com', 'hashed_password1', 1),
+                                             (2, 'user2@example.com', 'hashed_password2', 1),
+                                             (3, 'user3@example.com', 'hashed_password3', 1),
+                                             (4, 'admin@example.com', 'hashed_admin_password', 100),
+                                             (5, 'organizer@example.com', 'hashed_organizer_password', 2);
+
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ documents/Tableau de bord.md
@@ -0,0 +1,58 @@
+# Tableau de bord
+
+## Fonctionnalités
+
+### Fonctionnalités prioritaires
+
+- [X] **1**. Affichage de la liste des spectacles : pour chaque spectacle, on affiche le titre, la date et
+  l’horaire, une image
+  - Fait par : **David** 
+- [X] **5**. Affichage détaillé d’un spectacle : titre, artistes, description, style, durée, image(s),
+  extrait audio/vidéo
+  - Fait par : **Burak**
+- [X] **13**. S’authentifier
+  - Fait par : **David**
+- [X] **14**. Créer un spectacle : saisir les données et les valider
+  - Fait par : **Paul**
+
+### Fonctionnalités minimales attendues
+
+- [ ] **2**. Filtrage de la liste des spectacles par date,
+  - Fait par :
+- [X] **3**. Filtrage de la liste des spectacles par style de musique
+  - Fait par : **Burak**
+  - **Remarque** : Amélioration possible : Récupérer les styles de musique depuis la base de données et générer les filtres dynamiquement.
+- [ ] **4**. Filtrage de la liste des spectacles par lieu.
+  - Fait par : 
+- [ ] **6**. Affichage du détail d’une soirée : nom de la soirée, thématique, date et horaire, lieu,
+  tarifs, ainsi que la liste des spectacles : titre, artistes, description, style de musique, vidéo
+  - Fait par :
+- [X] **7**. En cliquant sur un spectacle dans la liste, le détail de la soirée correspondante est affiché
+  - Fait par : **Burak**
+- [ ] **15**. Créer une soirée : saisir les données et les valider
+  - Fait par : **Paul**
+- [ ] **16**. Ajouter un spectacle à une soirée
+  - Fait par :
+
+### Fonctionnalités étendues
+
+- [ ] **8**. En lien avec l’affichage d’un spectacle : accès aux spectacles du même lieu
+  - Fait par :
+- [ ] **9**. En lien avec l’affichage d’un spectacle : accès aux spectacles du même style
+  - Fait par :
+- [ ] **10**. En lien avec l’affichage d’un spectacle : accès aux spectacles à la même date
+  - Fait par :
+- [ ] **17**. Annuler un spectacle : le spectacle est conservé dans les affichages mais est marqué
+  comme annulé
+  - Fait par :
+- [ ] **18**. Modifier un spectacle existant
+  - Fait par :
+- [ ] **19**. Modifier les spectacles d’une soirée existante
+  - Fait par :
+- [ ] **20**. Créer un compte staff : créer un compte utilisateur permettant de gérer le programme
+  - Fait par :
+- [ ] **11**. Ajouter un spectacle dans sa liste de préférence (sans authentification ni création de
+  compte)
+  - Fait par :
+- [ ] **12**. Afficher sa liste de préférence
+  - Fait par :
